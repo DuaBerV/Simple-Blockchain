fae2cf91e2196923f82e791b2bca764642877cb7
Vadim Rytkov <vedimcreperf@gmail.com>
2025-10-07 22:29:24 +0300

    The blockchain is implemented

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,4 @@
+import java.security.MessageDigest;
 import java.util.Date;
 
 public class Block{
@@ -5,12 +6,29 @@ public class Block{
     public String previousHash;
     public String data;
     public long timeStamp; //number of milliseconds since 1/1/1970
+    private int nonce;
 
     //Block constructor
-    public Block(String hash, String previousHash){
-        this.hash = hash;
+    public Block(String data, String previousHash) throws Exception {
+        this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() throws Exception{
+        String calculatedhash = StringUtil.applySha256(
+                previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+        return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) throws Exception {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)){
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block mined: " + hash);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,52 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 6;
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the count of block you want to mine: ");
+        int howMany = input.nextInt();
+
+        for(int i = 0; i < howMany; i++) {
+            Instant start = Instant.now();
+            blockchain.add(new Block((i + 1) + " block", "0"));
+            System.out.println("Mine block " + (i + 1) + " ...");
+            blockchain.get(i).mineBlock(difficulty);
+            Instant end = Instant.now();
+            System.out.println(Duration.between(start, end).toSeconds() + " seconds");
+        }
+
+        System.out.println("Blockchain is Valid: " + isChainValid());
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("Blockchain JSON: " + blockchainJson);
+
+
+    }
+
+    public static Boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+
+        for(int i = 1; i < blockchain.size(); i++){
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            if(!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("Previous hashes not equal");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
